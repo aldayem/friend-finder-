ec942fd527acaa94ae953f322663d93369f26b32
ahmad aldayem <ahmad_abd_aldayem@hotmail.com>
2017-04-21 22:12:00 -0700

    Add files via upload

---
+++ app/data/friends.js
@@ -0,0 +1,15 @@
+//hold all of the data for the members
+
+var friends = [
+
+	{
+		name: "Danielle",
+		location:"Orlando",
+		age: "34",
+		photoLink: "",
+		score: "",		
+	},
+
+]
+
+module.exports = friends; //makes table data available to other pages
